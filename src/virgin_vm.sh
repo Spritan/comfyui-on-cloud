975641f4653ea0eefb5b7ae5d6ba7423e0d4dd38
Proyash Paban Sarma Borah <47718224+Spritan@users.noreply.github.com>
2025-01-28 12:03:12 +0530

    Update virgin_vm.sh

--- src/virgin_vm.sh
+++ src/virgin_vm.sh
@@ -31,6 +31,8 @@ wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/
 sudo dpkg -i cuda-repo-ubuntu2004-11-8-local_11.8.0-520.61.05-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2004-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
+wget http://security.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2ubuntu0.1_amd64.deb
+sudo apt install ./libtinfo5_6.3-2ubuntu0.1_amd64.deb
 sudo apt-get -y install cuda
 
 source .bashrc
